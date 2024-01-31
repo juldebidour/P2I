eee7b6f86df42416b2e2efc7f699d3ea007ac783
Julien Debidourjdebidour <jdebidour@ensc.fr>
2024-01-31 17:24:22 +0100

    Update 31 January

--- Esp_Server_Good/Esp_Server_Good.ino
+++ Esp_Server_Good/Esp_Server_Good.ino
@@ -7,145 +7,51 @@ const char* password = "password";
 ESP8266WebServer server(80);
 
 void handleRoot() {
-  String content = "<html><body>";
-  content += "<head>
-    <title><h1>Bienvenu au sein de AIRPURE_STATION </h1></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">;
-  content += "<p>Température actuelle : " + String(getTemperature()) + " &deg;C</p>";
-  content += "<p>Pression actuelle : " + String(getPressure()) + " Pa</p>";
-  content += "<p>Quantité de microparticules de taille 2,5 micromètres : " + String(getParticleCount()) + "</p>";
-  content += "<p>Quantité de microparticules de taille 10 micromètres : " + String(getParticleCount()) + "</p>";
-  content += "<style>
-        body,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-family: "Lato", sans-serif
-        }
-
-        .w3-bar,
-        h1,
-        button {
-            font-family: "Montserrat", sans-serif
-        }
-
-        .fa-anchor,
-        .fa-coffee {
-            font-size: 200px
-        }
-    </style></head>
-    <div class="w3-top">
-        <div class="w3-bar w3-red w3-card w3-left-align w3-large">
-            <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red"
-                href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i
-                    class="fa fa-bars"></i></a>
-            <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 1</a>
-            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 2</a>
-            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
-            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
-        </div>
-
-        <!-- Navbar on small screens -->
-        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-            <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
-        </div>
-    </div>
-
-    <!-- Header -->
-    <header class="w3-container w3-red w3-center" style="padding:128px 16px">
-        <h1 class="w3-margin w3-jumbo">START PAGE</h1>
-        <p class="w3-xlarge">Template by w3.css</p>
-        <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button>
-    </header>
-
-    <!-- First Grid -->
-    <div class="w3-row-padding w3-padding-64 w3-container">
-        <div class="w3-content">
-            <div class="w3-twothird">
-                <h1>Lorem Ipsum</h1>
-                <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
-
-                <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-
-            <div class="w3-third w3-center">
-                <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
-            </div>
-        </div>
-    </div>
-
-    <!-- Second Grid -->
-    <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
-        <div class="w3-content">
-            <div class="w3-third w3-center">
-                <i class="fa fa-coffee w3-padding-64 w3-text-red w3-margin-right"></i>
-            </div>
-
-            <div class="w3-twothird">
-                <h1>Lorem Ipsum</h1>
-                <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
-
-                <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-        <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
-    </div>
-
-    <!-- Footer -->
-    <footer class="w3-container w3-padding-64 w3-center w3-opacity">
-        <div class="w3-xlarge w3-padding-32">
-            <i class="fa fa-facebook-official w3-hover-opacity"></i>
-            <i class="fa fa-instagram w3-hover-opacity"></i>
-            <i class="fa fa-snapchat w3-hover-opacity"></i>
-            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-            <i class="fa fa-twitter w3-hover-opacity"></i>
-            <i class="fa fa-linkedin w3-hover-opacity"></i>
-        </div>
-        <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-    </footer>
-
-    <script>
-        function myFunction() {
-            var x = document.getElementById("navDemo");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-    </script>
-    </body></html>";
+  String content = "<html><head><title>Bienvenue sur AIRPURE_STATION</title>";
+  content += "<meta charset=\"UTF-8\">";
+  content += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  content += "<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">";
+  content += "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Lato\">";
+  content += "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Montserrat\">";
+  content += "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">";
+  content += "</head><body>";
+  content += "<div class=\"w3-top\">";
+  content += "<div class=\"w3-bar w3-red w3-card w3-left-align w3-large\">";
+  content += "<a class=\"w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red\" href=\"javascript:void(0);\" onclick=\"myFunction()\" title=\"Toggle Navigation Menu\"><i class=\"fa fa-bars\"></i></a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large w3-white\">The Airpure Project</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">The Airpure Weather Station</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Data</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Specifications</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Codes</a>";
+  content += "</div>";
+  content += "<div id=\"navDemo\" class=\"w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large\">";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">The Airpure Weather Station</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Data</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Specifications</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Codes</a>";
+  content += "</div></div>";
+  content += "<header class=\"w3-container w3-red w3-center\" style=\"padding:128px 16px\">";
+  content += "<h1 class=\"w3-margin w3-jumbo\">Welcome to the Airpure Station Project !</h1>";
+  content += "<p class=\"w3-xlarge\">a personal computing project by Julien Debidour/p>";
+  content += "<button class=\"w3-button w3-black w3-padding-large w3-large w3-margin-top\">See more informations !</button>";
+  content += "</header>";
+  content += "<div class=\"w3-row-padding w3-padding-64 w3-container\">";
+  content += "<div class=\"w3-content\"><div class=\"w3-twothird\"><h1>The Airpure Station Project in brief :</h1>";
+  content += "<h5 class=\"w3-padding-32\">This project aims to set up a connected station using gas sensors such as SDS 011, MQ135 or BMP 280, in particular to monitor air quality. It will allow access to data collected remotely by a remote server. This is a personal initiative that addresses a growing need for air quality monitoring. <br> A stand-alone weather and air composition station will be set up to monitor these different variables. <br> The aim to deliver are composed of a connected station integrating SDS 011 and MQ135 sensors, a web user/remote server interface to visualize the collected data, an a data storage system.</p>";
+  content += "</div><div class=\"w3-third w3-center\"><i class=\"fa fa-anchor w3-padding-64 w3-text-red\"></i></div>";
+  content += "</div></div><div class=\"w3-row-padding w3-light-grey w3-padding-64 w3-container\">";
+  content += "<div class=\"w3-content\"><div class=\"w3-third w3-center\"><i class=\"fa fa-coffee w3-padding-64 w3-text-red w3-margin-right\"></i></div>";
+  content += "<div class=\"w3-twothird\"><h1>Use cases and context :</h1><h5 class=\"w3-padding-32\">The station will be used in specific areas to monitor air quality. End users will be able to access the data via a secure web interface from anywhere, at any time. <br> It will collect and store air quality data, including :<ul><li><br> Measurements of SDS011 and MQ135 sensors. </li><br> <li>Data on air pollution.</li> <br> <li>Variations in detected gas concentrations.</li></p>";
+  content += "</div></div></div><div class=\"w3-container w3-black w3-center w3-opacity w3-padding-64\">";
+  content += "<h1 class=\"w3-margin w3-xlarge\">Quote of the day: Be safe, breathe better !</h1></div>";
+  content += "<footer class=\"w3-container w3-padding-64 w3-center w3-opacity\">";
+  content += "<div class=\"w3-xlarge w3-padding-32\"><i class=\"fa fa-facebook-official w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-instagram w3-hover-opacity\"></i><i class=\"fa fa-snapchat w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-pinterest-p w3-hover-opacity\"></i><i class=\"fa fa-twitter w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-linkedin w3-hover-opacity\"></i></div>";
+  content += "<p> Thanks for reading it ! I hope you liked it !</p>";
+  content += "</footer><script>function myFunction() { var x = document.getElementById(\"navDemo\");";
+  content += "if (x.className.indexOf(\"w3-show\") == -1) { x.className += \" w3-show\"; } else { x.className = x.className.replace(\" w3-show\", \"\"); }}</script></body></html>";
 
   server.send(200, "text/html", content);
 }
--- Wifi_WebSocket_Manager/ESP8266_Web_Server/ESP8266_Web_Server.ino
+++ Wifi_WebSocket_Manager/ESP8266_Web_Server/ESP8266_Web_Server.ino
@@ -1,139 +1,92 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include "LittleFS.h"
-#include <Arduino_JSON.h>
-#include <Adafruit_BME280.h>
-#include <Adafruit_Sensor.h>
+#include <ESP8266WebServer.h>
 
-// Replace with your network credentials
-const char* ssid = "Iphone de Julien";
-const char* password = "moulin24";
+const char* ssid = "AutoConnectAP";
+const char* password = "password";
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+ESP8266WebServer server(80);
 
-// Create a WebSocket object
-AsyncWebSocket ws("/ws");
+void handleRoot() {
+  String content = "<html><head><title>Bienvenue sur AIRPURE_STATION</title>";
+  content += "<meta charset=\"UTF-8\">";
+  content += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  content += "<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">";
+  content += "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Lato\">";
+  content += "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Montserrat\">";
+  content += "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">";
+  content += "</head><body>";
+  content += "<div class=\"w3-top\">";
+  content += "<div class=\"w3-bar w3-red w3-card w3-left-align w3-large\">";
+  content += "<a class=\"w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red\" href=\"javascript:void(0);\" onclick=\"myFunction()\" title=\"Toggle Navigation Menu\"><i class=\"fa fa-bars\"></i></a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large w3-white\">The Airpure Project</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">The Airpure Weather Station</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Data</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Specifications</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white\">Codes</a>";
+  content += "</div>";
+  content += "<div id=\"navDemo\" class=\"w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large\">";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">The Airpure Weather Station</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Data</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Specifications</a>";
+  content += "<a href=\"#\" class=\"w3-bar-item w3-button w3-padding-large\">Codes</a>";
+  content += "</div></div>";
+  content += "<header class=\"w3-container w3-red w3-center\" style=\"padding:128px 16px\">";
+  content += "<h1 class=\"w3-margin w3-jumbo\">Welcome to the Airpure Station Project !</h1>";
+  content += "<p class=\"w3-xlarge\">a personal computing project by Julien Debidour/p>";
+  content += "<button class=\"w3-button w3-black w3-padding-large w3-large w3-margin-top\">See more informations !</button>";
+  content += "</header>";
+  content += "<div class=\"w3-row-padding w3-padding-64 w3-container\">";
+  content += "<div class=\"w3-content\"><div class=\"w3-twothird\"><h1>The Airpure Station Project in brief :</h1>";
+  content += "<h5 class=\"w3-padding-32\">This project aims to set up a connected station using gas sensors such as SDS 011, MQ135 or BMP 280, in particular to monitor air quality. It will allow access to data collected remotely by a remote server. This is a personal initiative that addresses a growing need for air quality monitoring. <br> A stand-alone weather and air composition station will be set up to monitor these different variables. <br> The aim to deliver are composed of a connected station integrating SDS 011 and MQ135 sensors, a web user/remote server interface to visualize the collected data, an a data storage system.</p>";
+  content += "</div><div class=\"w3-third w3-center\"><i class=\"fa fa-anchor w3-padding-64 w3-text-red\"></i></div>";
+  content += "</div></div><div class=\"w3-row-padding w3-light-grey w3-padding-64 w3-container\">";
+  content += "<div class=\"w3-content\"><div class=\"w3-third w3-center\"><i class=\"fa fa-coffee w3-padding-64 w3-text-red w3-margin-right\"></i></div>";
+  content += "<div class=\"w3-twothird\"><h1>Use cases and context :</h1><h5 class=\"w3-padding-32\">The station will be used in specific areas to monitor air quality. End users will be able to access the data via a secure web interface from anywhere, at any time. <br> It will collect and store air quality data, including :<ul><li><br> Measurements of SDS011 and MQ135 sensors. </li><br> <li>Data on air pollution.</li> <br> <li>Variations in detected gas concentrations.</li></p>";
+  content += "</div></div></div><div class=\"w3-container w3-black w3-center w3-opacity w3-padding-64\">";
+  content += "<h1 class=\"w3-margin w3-xlarge\">Quote of the day: Be safe, breathe better !</h1></div>";
+  content += "<footer class=\"w3-container w3-padding-64 w3-center w3-opacity\">";
+  content += "<div class=\"w3-xlarge w3-padding-32\"><i class=\"fa fa-facebook-official w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-instagram w3-hover-opacity\"></i><i class=\"fa fa-snapchat w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-pinterest-p w3-hover-opacity\"></i><i class=\"fa fa-twitter w3-hover-opacity\"></i>";
+  content += "<i class=\"fa fa-linkedin w3-hover-opacity\"></i></div>";
+  content += "<p> Thanks for reading it ! I hope you liked it !</p>";
+  content += "</footer><script>function myFunction() { var x = document.getElementById(\"navDemo\");";
+  content += "if (x.className.indexOf(\"w3-show\") == -1) { x.className += \" w3-show\"; } else { x.className = x.className.replace(\" w3-show\", \"\"); }}</script></body></html>";
 
-// Json Variable to Hold Sensor Readings
-JSONVar readings;
-
-// Timer variables
-unsigned long lastTime = 0;  
-unsigned long timerDelay = 30000;
-
-// Create a sensor object
-Adafruit_BME280 bme;         // BME280 connect to ESP32 I2C (GPIO 21 = SDA, GPIO 22 = SCL)
-
-// Init BME280
-void initBME(){
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
+  server.send(200, "text/html", content);
 }
 
-// Get Sensor Readings and return JSON object
-String getSensorReadings(){
-  readings["temperature"] = String(bme.readTemperature());
-  readings["humidity"] =  String(bme.readHumidity());
-  readings["pressure"] = String(bme.readPressure()/100.0F);
-  String jsonString = JSON.stringify(readings);
-  return jsonString;
+float getTemperature() {
+  // Replace this with your BMP280 temperature reading logic
+  return 25.5;
 }
 
-// Initialize LittleFS
-void initFS() {
-  if (!LittleFS.begin()) {
-    Serial.println("An error has occurred while mounting LittleFS");
-  }
-  else{
-   Serial.println("LittleFS mounted successfully");
-  }
+float getPressure() {
+  // Replace this with your BMP280 pressure reading logic
+  return 101325.0;
 }
 
-// Initialize WiFi
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
-
-void notifyClients(String sensorReadings) {
-  ws.textAll(sensorReadings);
-}
-
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    //data[len] = 0;
-    //String message = (char*)data;
-    // Check if the message is "getReadings"
-    //if (strcmp((char*)data, "getReadings") == 0) {
-      //if it is, send current sensor readings
-      String sensorReadings = getSensorReadings();
-      Serial.print(sensorReadings);
-      notifyClients(sensorReadings);
-    //}
-  }
-}
-
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
-  }
-}
-
-void initWebSocket() {
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
+int getParticleCount() {
+  // Replace this with your SDS011 particle count reading logic
+  return 50;
 }
 
 void setup() {
   Serial.begin(115200);
-  initBME();
-  initWiFi();
-  initFS();
-  initWebSocket();
 
-  // Web Server Root URL
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/index.html", "text/html");
-  });
+  // Connect to WiFi
+  WiFi.softAP(ssid, password);
+  Serial.println("WiFi AP Started");
 
-  server.serveStatic("/", LittleFS, "/");
+  // Define server routes
+  server.on("/", HTTP_GET, handleRoot);
 
   // Start server
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
-    String sensorReadings = getSensorReadings();
-    Serial.print(sensorReadings);
-    notifyClients(sensorReadings);
-
-  lastTime = millis();
-
-  }
-
-  ws.cleanupClients();
+  // Handle incoming client requests
+  server.handleClient();
 }
